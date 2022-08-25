93f92292985cf71f368f5436124a4f903942b72a
Yutta <111496203+Yutta-u@users.noreply.github.com>
2022-08-25 17:38:42 +0500

    Initial commit

---
+++ src/crane.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class crane {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+
+        int x = a / 6;
+
+       int p = x;
+       int k = (x + x) * 2;
+       int s = p;
+
+        System.out.println(p);
+        System.out.println(k);
+        System.out.println(s);
+    }
+}
